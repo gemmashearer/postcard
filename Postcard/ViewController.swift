06197d81d0cda866ba9246eee5149766c21c607b
Gemma Shearer <gemmaandjude@gmail.com>
2015-05-28 18:23:40 +0100

    First commit of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var sendMessage: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+   
+    @IBAction func sendMessageButton(sender: UIButton) {
+        messageLabel.hidden = false
+        enterMessageTextField.resignFirstResponder()
+        enterMessageTextField.text = ""
+        sendMessage.setTitle("Message sent!", forState: UIControlState.Normal)
+    }
+   
+   
+   }
 
